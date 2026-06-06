88b12be4069ad64f6b7cc47c919ad32ef8e43daf
mobiseccloud <mobisec.cloud@gmail.com>
2026-06-06 10:33:09 +0200

    Update processPacketNative to include pcapFd parameter

--- app/src/main/java/com/mobisec/omniip/core/NativeEngine.kt
+++ app/src/main/java/com/mobisec/omniip/core/NativeEngine.kt
@@ -12,7 +12,7 @@ object NativeEngine {
     external fun initializeNativeEnvironment(isDebug: Boolean)
     external fun setPremiumUnlockedNative(unlocked: Boolean)
     external fun executeSecuritySweep(context: android.content.Context)
-    external fun processPacketNative(packetBuffer: java.nio.ByteBuffer, length: Int): Int
+    external fun processPacketNative(packetBuffer: java.nio.ByteBuffer, length: Int, pcapFd: Int): Int
 
     external fun updateNativeRule(key: String, ip: Int, port: Int, action: Int)
     external fun updateNativeRule(uid: Int, direction: Int, interfaceType: Int, isBlocked: Boolean)
